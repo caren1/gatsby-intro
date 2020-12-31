b1636033d7c3d5e6bf8f6b03b0296bf0603a1eee
caren1 <czarnocki.w@wp.pl>
2020-12-31 18:12:39 +0100

    CSS modules in gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
 
+// css modules practice => recommended way for gatsby
+import headerStyles from './header.module.scss'
+
 const Header = () => {
   return (
-    <header>
-      <h1>GatsbyJS Bootcamp!</h1>
+    <header className={headerStyles.header}>
+      <h1><Link to="/" className={headerStyles.title}>GatsbyJS Bootcamp!</Link></h1>
       <nav>
-        <ul>
-          <Link to="/">
+        <ul className={headerStyles.navList}>
+          <Link to="/" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
             <li>Home</li>
           </Link>
-          <Link to="/blog">
+          <Link to="/blog" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
             <li>Blog</li>
           </Link>
-          <Link to="/about">
+          <Link to="/about" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
             <li>About</li>
           </Link>
-          <Link to="/contact">
+          <Link to="/contact" className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}>
             <li>Contact</li>
           </Link>
         </ul>
