2529a60f0ce77381e5b6983bfe9af0b70bd6b5ec
caren1 <czarnocki.w@wp.pl>
2021-01-01 17:54:04 +0100

    generating blog posts based on template and slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,6 +8,10 @@
 // slug will be taken from filename, that is why we'll use a special
 // in-built npm package that allows for such functionality - PATH (node.js)
 
+// for single blog post we created a new directory 'templates' where we store a regular react component
+
+// for our 3rd goal, so actually rendering the separate blog pages we'll need another api from inside gatsby-nodeconfig file -> createPages
+
 const path = require("path")
 
 module.exports.onCreateNode = ({ node, actions }) => {
@@ -20,4 +24,35 @@ module.exports.onCreateNode = ({ node, actions }) => {
     // in this way we achieve goal1 to create a slug for each post
     createNodeField({ node, name: "slug", value: slug })
   }
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  // 1 - get path to template - absolute path from harddrive
+  const blogtemplate = path.resolve(`./src/templates/blog.js`)
+  // 2 - get markdown data
+  const response = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  // 3 - create new pages
+  response.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogtemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 
-import { graphql, useStaticQuery } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -13,8 +13,9 @@ const BlogPage = () => {
               title
               date
             }
-            html
-            excerpt
+            fields {
+              slug
+            }
           }
         }
       }
@@ -27,8 +28,10 @@ const BlogPage = () => {
       <ol>
         {data.allMarkdownRemark.edges.map(edge => (
           <li key={edge.node.frontmatter.title}>
-            <h2>{edge.node.frontmatter.title}</h2>
-            <p>{edge.node.frontmatter.date}</p>
+            <Link to={`/blog/${edge.node.fields.slug}`}>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </Link>
           </li>
         ))}
       </ol>
