a2fd34601ceb411a271ea856590ca2d33aa4f0c7
caren1 <czarnocki.w@wp.pl>
2021-01-26 18:53:06 +0100

    generating list of blogs posts from contentful

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,16 +7,16 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
     query {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+      allContentfulBlogPost(
+        sort: {
+          fields: publishedDate,
+          order:DESC
+        }){
+        edges{
+          node{
+            title
+            slug
+            publishedDate(formatString: "MMMM Do. YYYY")
           }
         }
       }
@@ -27,11 +27,11 @@ const BlogPage = () => {
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map(edge => (
-          <li key={edge.node.frontmatter.title} className={blogStyles.post}>
-            <Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+        {data.allContentfulBlogPost.edges.map((edge) => (
+          <li key={edge.node.title} className={blogStyles.post}>
+            <Link to={`/blog/${edge.node.slug}`}>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.publishedDate}</p>
             </Link>
           </li>
         ))}
